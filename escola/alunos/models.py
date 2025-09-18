e3971f2e42058c509a0c53a8da301b26472b4b77
cauegrazziotinborga <cauegrazziotinborga@gmail.com>
2025-09-18 08:01:45 -0300

    html sendo exibido no navegador

--- escola/alunos/models.py
+++ escola/alunos/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-# Create your models here.
 class Aluno(models.Model):
     nome = models.CharField(max_length=200)
-    sobreno = models.CharField(max_length=200)
+    sobrenome = models.CharField(max_length=200)
     data_nascimento = models.CharField(max_length=10)
     curso = models.CharField(max_length=200)
